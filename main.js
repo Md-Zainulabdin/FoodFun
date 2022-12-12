3da15456a34c6217a33cc7c07ff532a4cff39d4f
Muhammad Zain-ul-Abdin Ansari <114089559+Md-Zainulabdin@users.noreply.github.com>
2022-12-12 08:27:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const bars = document.getElementById('bars');
+const listItems = document.querySelector('.menus');
+const link = document.querySelector('hover-link');
+
+function baropen() {
+
+    listItems.style.left = "0%";
+}
+function barclose() {
+    listItems.style.left = "-100%"
+};
+
+
